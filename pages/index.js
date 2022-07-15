bcffea9cb7f9f7be38fb590300e086851a4b09a6
Mike Bartels <mikebartels201@outlook.com>
2022-07-15 17:34:03 +0200

    Main component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 
 //Hello World
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <Main />
     </div>
   );
 }
