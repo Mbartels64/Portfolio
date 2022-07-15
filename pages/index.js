c6d14be3d94ff65929441a76b3329b93d32b2e4c
Mike Bartels <mikebartels201@outlook.com>
2022-07-15 21:26:06 +0200

    skills section

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import About from "../components/About";
 import Main from "../components/Main";
 import Navbar from "../components/Navbar";
+import Skills from "../components/Skills";
 
 //Hello World
 
@@ -17,6 +18,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Skills />
     </div>
   );
 }
