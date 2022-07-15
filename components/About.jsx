8e442046ae324abcec0390402deb2432ab555052
Mike Bartels <mikebartels201@outlook.com>
2022-07-15 22:05:55 +0200

    adjusting

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             About
           </p>
           <h2 className="py-4">Who I Am</h2>
-          <p className="py-2 text-gray-600">//short description</p>
+          <p className="py-2 text-gray-600">short description</p>
           <p className="py-2 text-gray-600">
             bio....... Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Est consequatur sed quasi eveniet, rerum aut porro nostrum ipsa
@@ -30,7 +30,7 @@ const About = () => {
           <img
             className="rounded-xl"
             src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80"
-            alt=""
+            alt="/"
           />
         </div>
       </div>
