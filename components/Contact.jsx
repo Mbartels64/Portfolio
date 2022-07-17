ea0f9abd2ad28e5c5e6a31adb347eb0786387259
Mike Bartels <mikebartels201@outlook.com>
2022-07-17 18:51:15 +0200

    small tweak

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <h2 className="py-2">Name Here</h2>
+                <h2 className="py-2">Mike Bartels</h2>
                 <p>Full-Stack Developer</p>
                 <p className="py-4">
                   I am available for freelance or full-time positions. Contact
