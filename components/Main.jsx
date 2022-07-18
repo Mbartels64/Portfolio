ba3a67ec5e9d96ab65804b9a5b9cc9066ee62f8c
Mike Bartels <mikebartels201@outlook.com>
2022-07-18 16:29:41 +0200

    main

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Typed from "react-typed";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
@@ -12,9 +13,20 @@ const Main = () => {
           <p className="uppercase text-sm tracking-widest text-gray-600">
             LET&#39;S BUILD SOMETHNG TOGETHER
           </p>
-          <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-[#5651e5]">Mike</span>
-          </h1>
+          <div>
+            <h1 className="py-4 text-gray-700">
+              Hi, I&#39;m
+              <span>
+                <Typed
+                  className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 text-[#5651e5]"
+                  strings={["Mike", "Bartels"]}
+                  typeSpeed={120}
+                  backSpeed={140}
+                  loop
+                />
+              </span>
+            </h1>
+          </div>
           <h1 className="py-2 text-gray-700">A Full Stack Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
             I&#39;m a Full Stack Developer I curruntly specialize in Web
