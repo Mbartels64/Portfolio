6ca7faffd4bc3dcc59b70a8316381daec5e7c0b0
Mike Bartels <mikebartels201@outlook.com>
2022-07-19 19:47:46 +0200

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,17 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BXTNTE2B3Q"
+      />
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BXTNTE2B3Q');`}
+      </script>
       <Navbar />
       <Component {...pageProps} />
     </>
