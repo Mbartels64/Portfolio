8d0fece833525f2264da689bd6dec5ba21d8e533
Mike Bartels <mikebartels201@outlook.com>
2022-07-19 20:09:16 +0200

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,6 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-BXTNTE2B3Q"
-      />
-      <script>
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BXTNTE2B3Q');`}
-      </script>
       <Navbar />
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,26 @@ import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import Script from "next/script";
 
 //Hello World
 
 export default function Home() {
   return (
     <div>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BXTNTE2B3Q"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BXTNTE2B3Q');
+        `}
+      </Script>
       <Head>
         <title>Mike Bartels | Full Stack Developer </title>
         <meta
@@ -20,6 +34,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Main />
       <About />
       <Skills />
